00821ba8d2c27ef9dd1ec39285ed2066b69f4d1b
Wandzzz <135227469+Wandiile@users.noreply.github.com>
2024-07-23 13:51:43 +0200

    Add files via upload

---
+++ isPalindromeChecker/Controllers/isPalindromeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace isPalindromeChecker.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class isPalindromeController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult PalindromeChecker([FromBody]string mom)
+        {
+            if (string.IsNullOrEmpty(mom))
+            {
+                return BadRequest("Input string is required");
+
+            }
+            string reversed = ReverseString(mom);
+            bool isPalindrome = IsPalindrome(mom, reversed);
+
+            return Ok(new { Reversed = reversed, IsPalindrome = isPalindrome });
+        }
+        private string ReverseString(string mom)
+        {
+            char[] alpha = mom.ToCharArray();
+            Array.Reverse(alpha);
+            return new string(alpha);
+        }
+
+        private bool IsPalindrome(string mom, string reversed)
+        {
+            return mom.Equals(reversed, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
